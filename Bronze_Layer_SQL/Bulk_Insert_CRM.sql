418e7c29f7315f8c7e3256dd9a2e0f34c9f10712
OFFICEGUYLPTP\bobrt <bobrtvlol@gmail.com>
2026-07-19 01:38:48 +0300

    Minor additions to bronze load query.

--- Bronze_Layer_SQL/Bulk_Insert_CRM.sql
+++ Bronze_Layer_SQL/Bulk_Insert_CRM.sql
@@ -1,51 +1,79 @@
 /*
 Use this query to bulk insert data into tables from CRM files.
 */
-
-
--- Local machine bulk insert into crm_cust_info.
-TRUNCATE TABLE bronze.crm_cust_info;
+-- Just in case
+USE DataWarehouseDB
 GO
 
-BULK INSERT bronze.crm_cust_info
-FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+CREATE OR ALTER PROCEDURE bronze.load_bronze_crm AS
+BEGIN	
+	DECLARE @exec_start_time DATETIME, @exec_end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		PRINT 'CRM LOAD'
+		SET @batch_start_time = GETDATE()
+		--------
+		-- Local machine bulk insert into crm_cust_info.
+		TRUNCATE TABLE bronze.crm_cust_info;
 
-SELECT * FROM bronze.crm_cust_info;
-SELECT COUNT(*) FROM bronze.crm_cust_info;
+		SET @exec_start_time = GETDATE()
+		BULK INSERT bronze.crm_cust_info
+		FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @exec_end_time = GETDATE()
+		PRINT 'crm_cust_info LOAD DURATION: ' + CAST(DATEDIFF(millisecond, @exec_start_time, @exec_end_time) AS NVARCHAR) + ' MILLISEC'
 
-
--- Local machine bulk insert into crm_prd_info.
-TRUNCATE TABLE bronze.crm_prd_info;
-GO
-
-BULK INSERT bronze.crm_prd_info
-FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_crm\prd_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-SELECT * FROM bronze.crm_prd_info;
-SELECT COUNT(*) FROM bronze.crm_prd_info;
+		--SELECT TOP 100 * FROM bronze.crm_cust_info;
+		--SELECT COUNT(*) FROM bronze.crm_cust_info;
 
 
--- Local machine bulk insert into crm_sales_details.
-TRUNCATE TABLE bronze.crm_sales_details;
-GO
+		--------
+		-- Local machine bulk insert into crm_prd_info.
+		TRUNCATE TABLE bronze.crm_prd_info;
 
-BULK INSERT bronze.crm_sales_details
-FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+		SET @exec_start_time = GETDATE()
+		BULK INSERT bronze.crm_prd_info
+		FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @exec_end_time = GETDATE()
+		PRINT 'crm_prd_info LOAD DURATION: ' + CAST(DATEDIFF(millisecond, @exec_start_time, @exec_end_time) AS NVARCHAR) + ' MILLISEC'
 
-SELECT * FROM bronze.crm_sales_details;
-SELECT COUNT(*) FROM bronze.crm_sales_details;
+		--SELECT TOP 100 * FROM bronze.crm_prd_info;
+		--SELECT COUNT(*) FROM bronze.crm_prd_info;
+
+
+		--------
+		-- Local machine bulk insert into crm_sales_details.
+		TRUNCATE TABLE bronze.crm_sales_details;
+
+		SET @exec_start_time = GETDATE()
+		BULK INSERT bronze.crm_sales_details
+		FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @exec_end_time = GETDATE()
+		PRINT 'crm_sales_details LOAD DURATION: ' + CAST(DATEDIFF(millisecond, @exec_start_time, @exec_end_time) AS NVARCHAR) + ' MILLISEC'
+
+		--SELECT TOP 100 * FROM bronze.crm_sales_details;
+		--SELECT COUNT(*) FROM bronze.crm_sales_details;
+		SET @batch_end_time = GETDATE()
+		PRINT 'WHOLE CRM BATCH LOAD DURATION: ' + CAST(DATEDIFF(millisecond, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' MILLISEC'
+
+	END TRY
+	BEGIN CATCH
+		PRINT 'CRM LOAD FAILED'
+		PRINT 'Error:' + ERROR_MESSAGE();
+		PRINT CAST(ERROR_NUMBER() AS NVARCHAR)
+		PRINT CAST(ERROR_STATE() AS NVARCHAR)
+	END CATCH
+END
--- Bronze_Layer_SQL/Bulk_Insert_ERP.sql
+++ Bronze_Layer_SQL/Bulk_Insert_ERP.sql
@@ -1,51 +1,80 @@
 /*
 Use this query to bulk insert data into tables from ERP files.
 */
-
-
--- Local machine bulk insert into erp_cust_az12.
-TRUNCATE TABLE bronze.erp_cust_az12;
+-- Just in case
+USE DataWarehouseDB
 GO
 
-BULK INSERT bronze.erp_cust_az12
-FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_erp\CUST_AZ12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+CREATE OR ALTER PROCEDURE bronze.load_bronze_erp AS
+BEGIN
+	DECLARE @exec_start_time DATETIME, @exec_end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		PRINT 'ERP LOAD'
+		SET @batch_start_time = GETDATE()
+		--------
+		-- Local machine bulk insert into erp_cust_az12.
+		TRUNCATE TABLE bronze.erp_cust_az12
 
-SELECT * FROM bronze.erp_cust_az12;
-SELECT COUNT(*) FROM bronze.erp_cust_az12;
+		SET @exec_start_time = GETDATE()
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @exec_end_time = GETDATE()
+		PRINT 'CUST_AZ12 LOAD DURATION: ' + CAST(DATEDIFF(millisecond, @exec_start_time, @exec_end_time) AS NVARCHAR) + ' MILLISEC'
 
-
--- Local machine bulk insert into erp_loc_a101.
-TRUNCATE TABLE bronze.erp_loc_a101;
-GO
-
-BULK INSERT bronze.erp_loc_a101
-FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_erp\LOC_A101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-SELECT * FROM bronze.erp_loc_a101;
-SELECT COUNT(*) FROM bronze.erp_loc_a101;
+		--SELECT TOP 100 * FROM bronze.erp_cust_az12;
+		--SELECT COUNT(*) FROM bronze.erp_cust_az12;
 
 
--- Local machine bulk insert into erp_px_cat_g1v2.
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-GO
+		--------
+		-- Local machine bulk insert into erp_loc_a101.
+		TRUNCATE TABLE bronze.erp_loc_a101;
 
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_erp\PX_CAT_G1V2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+		SET @exec_start_time = GETDATE()
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @exec_end_time = GETDATE()
+		PRINT 'erp_loc_a101 LOAD DURATION: ' + CAST(DATEDIFF(millisecond, @exec_start_time, @exec_end_time) AS NVARCHAR) + ' MILLISEC'
 
-SELECT * FROM bronze.erp_px_cat_g1v2;
-SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2;
+		--SELECT TOP 100 * FROM bronze.erp_loc_a101;
+		--SELECT COUNT(*) FROM bronze.erp_loc_a101;
+
+
+		--------
+		-- Local machine bulk insert into erp_px_cat_g1v2.
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+		SET @exec_start_time = GETDATE()
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'D:\Projets_Git_Aggregated\DWH_MS.SQL.Server\Data_Source\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @exec_end_time = GETDATE()
+		PRINT 'erp_px_cat_g1v2 LOAD DURATION: ' + CAST(DATEDIFF(millisecond, @exec_start_time, @exec_end_time) AS NVARCHAR) + ' MILLISEC'
+
+		--SELECT TOP 100 * FROM bronze.erp_px_cat_g1v2;
+		--COUNT(*) FROM bronze.erp_px_cat_g1v2;
+
+		SET @batch_end_time = GETDATE()
+		PRINT 'WHOLE ERP BATCH LOAD DURATION: ' + CAST(DATEDIFF(millisecond, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' MILLISEC'
+
+	END TRY
+	BEGIN CATCH
+		PRINT 'ERP LOAD FAIL'
+		PRINT 'Error:' + ERROR_MESSAGE();
+		PRINT CAST(ERROR_NUMBER() AS NVARCHAR)
+		PRINT CAST(ERROR_STATE() AS NVARCHAR)
+	END CATCH
+END
